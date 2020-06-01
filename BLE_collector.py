a621181f2a260fb0d45b21f6cd25fe890e9575a2
Andre Clarke <andr9e@gmail.com>
2020-06-01 18:12:56 -0400

    smartMoistureprobe table add

--- BLE_collector.py
+++ BLE_collector.py
@@ -17,10 +17,10 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a Published message is received from the server
 def on_message(client, userdata, msg):
-	print ("New message Received"+msg)
 	selectTagToPub(json.loads(msg.payload)["tagType"],client,msg.payload)
 
 def is_EnvironmentSensor(client,msg):
+	print (msg)
 	timestampUTC = str(json.loads(msg)["timestampUTC"])
 	router_mac =str(json.loads(msg)["router_mac"])
 	router_lat=float(json.loads(msg)["router_lat"])
@@ -46,7 +46,7 @@ def is_EnvironmentSensor(client,msg):
 		"MRAPFrameCount,routerDeviceCount,routerMajor,routerMinor) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(timestampUTC, router_mac,router_lat,router_long,rssi,
 		Temperature,Humidity,Lux,uvPower,pressure,deviceAddr,MrapFrameCount,router_deviceCount,router_major,router_minor)) #Insert a row of data
 	except Error as e:
-		print(e)
+		print (e)
 	conn.commit() #save the changes
 	conn.close() #close the connection to the db
 	#client.publish("EnvironmentSensor/Temperature",Temperature)
@@ -55,8 +55,47 @@ def is_EnvironmentSensor(client,msg):
 	#print ("Published Enviornment Temperature Data:"+ Temperature)
 
 def is_SmartMoistureProbe(client, msg):
-	Index = json.loads(msg)["Index"]
-	client.publish("SmartMoistureProbe/Index", Index)
+	print (msg)
+#	timestampUTC = str(json.loads(msg)["timestampUTC"])
+#	router_mac = str(json.loads(msg)["router_mac"])
+#	dev_type = int(json.loads(msg)["dev_type"])
+#	router_tl = str(json.loads(msg)["router_tl"])
+#	router_tlid = str(json.loads(msg)["router_tlid"])
+#	router_lat= float(json.loads(msg)["router_lat"])
+#	router_long= float(json.loads(msg)["router_long"])
+#	nodeAddr = str(json.loads(msg)["nodeAddr"])
+#	router_deviceCount=int(json.loads(msg)["router_deviceCount"])
+#	deviceAddr= str(json.loads(msg)["deviceAddr"])
+#	rssi=int(json.loads(msg)["rssi"])
+#	tagType = str(json.loads(msg)["tagType"])
+#	version = str(json.loads(msg)["version"])
+#	MrapFrameCount= int(json.loads(msg)["MrapFrameCount"])
+#	Index = int(json.loads(msg)["Index"])
+#	Temperature =int(json.loads(msg)["Temperature"])
+#	uvPower = float(json.loads(msg)["uvPower"])
+#	VisibleLightPower = float(json.loads(msg)["VisibleLightPower"])
+#	router_major=int(json.loads(msg)["router_major"])
+#	router_minor=int(json.loads(msg)["router_minor"])
+
+	try:
+		conn = sqlite3.connect('bleSensor.db') #create a connection to database
+		a = conn.cursor()
+		a.execute("CREATE TABLE smartMoistureProbe (timestampUTC str,routerMac str,devType int,routerTl str,routerTLId str,routerLat float,"
+		"routerLong float,nodeAddr str,routerDeviceCount int,deviceAddress str,rssi int,tagType str,version str,MRAPFrameCount int,index int,"
+		"temperature int,uvPower float,visibleLightPower float,routerMajor int,routerMinor int)") #create table if does not exists
+	except OSError as err:
+		print ("Error: {0}".format(err))
+	try:
+		a.execute("INSERT INTO smartMoistureProbe (timestampUTC,routerMac,devType,routerTL,routerTLId,routerLat,routerLong,nodeAddr,routerDeviceCount,"
+		"deviceAddress,rssi,tagType,version,MRAPFrameCount,index,temperature,uvPower,visibleLightPower,"
+		"routerMajor,routerMinor) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(timestampUTC,router_mac,dev_type,router_tl,router_tlid,
+		router_lat,router_long,nodeAddr,router_deviceCount,deviceAddr,rssi,tagType,version,MrapFrameCount,Index,Temperature,
+		uvPower,VisibleLightPower,router_major,router_minor)) #Insert a row of data
+	except Error as e:
+		print (e)
+	conn.commit() #save the changes
+	conn.close() #close the connection to the db
+	#client.publish("SmartMoistureProbe/Index", Index)
 
 def selectTagToPub(tagType,client,msg):
 	tagList[tagType](client,msg)
@@ -72,6 +111,7 @@ client.on_message = on_message
 print ("connecting to broker")
 client.connect(broker_address) #connect to broker
 print ("Publishing message to topic","console/cmd")
-postInterval_scanDuration="{\"postInterval\":\"300000\",\"scanDuration\":\"300000\"}"
+#postInterval_scanDuration="{\"postInterval\":\"300000\",\"scanDuration\":\"300000\"}"
+postInterval_scanDuration="{\"postInterval\":\"60000\",\"scanDuration\":\"60000\"}"
 client.publish("console/cmd",postInterval_scanDuration) #set post interval (scan for 5 minutes , post every 5 minutes
 client.loop_forever()
