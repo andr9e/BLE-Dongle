34ce8576ea476a24df137ea0e7d5e6ef5223a200
Andre Clarke <andr9e@gmail.com>
2020-06-05 00:28:28 -0400

    update webapp.py

---
+++ webapp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
+import model
+
+app = Flask(__name__)
+
+# set optional bootswatch theme
+#app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+app.config['SECRET_KEY'] = 'mosfet'
+
+admin = Admin(app, name='SqliteDatabase', template_mode='bootstrap3', url='/')
+admin.add_view(ModelView(model.environmentSensor))
+admin.add_view(ModelView(model.smartMoistureProbe))
+# Add administrative views here
+
+app.run(host= '0.0.0.0')
